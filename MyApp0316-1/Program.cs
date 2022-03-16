e06b1613a040040317aaf06a35261f047c05e6af
yangho1123 <C107181103@nkust.edu.tw>
2022-03-16 16:13:34 +0800

    加入專案檔案。

---
+++ MyApp0316-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp0316_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入您的分數：");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 60 ? "及格" : "不及格";
+
+            Console.WriteLine("何同學，這次你{0}",result);
+            Console.ReadLine();
+        }
+    }
+}
